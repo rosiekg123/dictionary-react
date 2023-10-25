59962f61095628a8a14f77b80a4cf1aff7d034de
rosiekg123 <rosiekg1237@googlemail.com>
2023-10-25 20:08:46 +0100

    update api call

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,13 +6,12 @@ export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
 
-    alert(`Searching for ${keyword} definition`);
     let apiKey = `664efb8a30c957f12bbot9a042bca6c7`;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
