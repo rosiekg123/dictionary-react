9121755f57b5599cebc9f50174dcc586b880233f
rosiekg123 <rosiekg1237@googlemail.com>
2023-10-25 18:50:44 +0100

    initial set up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./dictionary picture one.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" className="img-fluid" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
