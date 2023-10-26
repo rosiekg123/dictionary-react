5a12f2fb9dfc43251d04c593ed2f99d2db3a44f9
rosiekg123 <rosiekg1237@googlemail.com>
2023-10-26 21:27:06 +0100

    addition of alt

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,12 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-md-4" key={index}>
-                <a href={photo.src.original} target="_blank" rel="nonreferrer">
+                <a
+                  href={photo.src.original}
+                  target="_blank"
+                  rel="nonreferrer"
+                  alt="photo"
+                >
                   {" "}
                   <img src={photo.src.landscape} className="img-fluid" />
                 </a>
