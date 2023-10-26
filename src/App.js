792c532051d4d3581344567971f064074f644bb2
rosiekg123 <rosiekg1237@googlemail.com>
2023-10-26 21:45:02 +0100

    updated footer

--- src/App.js
+++ src/App.js
@@ -12,7 +12,33 @@ function App() {
         <main>
           <Dictionary defaultKeyword="learn" />
         </main>
-        <footer className="text-center"> Coded by Rosie Miller</footer>
+        <footer className="text-center">
+          {" "}
+          This project was coded by &nbsp;
+          <a
+            href="https://effortless-crepe-2f841e.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Rosie Miller&nbsp;
+          </a>
+          and is&nbsp;
+          <a
+            href="https://github.com/rosiekg123?tab=repositories"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub&nbsp;
+          </a>
+          and hosted on&nbsp;
+          <a
+            href="https://brilliant-eclair-319c6e.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
