f8613e1eb165824a12d7474aac177cea8448f831
rosiekg123 <rosiekg1237@googlemail.com>
2023-10-26 21:37:12 +0100

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -14,10 +14,9 @@ export default function Photos(props) {
                   href={photo.src.original}
                   target="_blank"
                   rel="noopener noreferrer"
-                  alt=""
                 >
                   {" "}
-                  <img src={photo.src.landscape} className="img-fluid" />
+                  <img src={photo.src.landscape} className="img-fluid" alt="" />
                 </a>
               </div>
             );
